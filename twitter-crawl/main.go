433938d26337bf58d78316adfa80881c0f2b45e5
0x60018 <0x60018@gmail.com>
2023-11-27 20:06:19 +0800

    chore: add twitter login

--- twitter-crawl/main.go
+++ twitter-crawl/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"strings"
 	"time"
@@ -40,11 +42,8 @@ func connectDB() (db *gorm.DB) {
 	return db
 }
 
-func scan(tweetQuery string) {
-	scraper := twitterscraper.New()
-	scraper.SetSearchMode(twitterscraper.SearchLatest)
-
-	for tweet := range scraper.SearchTweets(context.Background(), tweetQuery, 100) {
+func scan(scraper *twitterscraper.Scraper, tweetQuery string) {
+	for tweet := range scraper.SearchTweets(context.Background(), tweetQuery, 10) {
 		if tweet.Error != nil {
 			continue
 		}
@@ -67,19 +66,81 @@ func scan(tweetQuery string) {
 	}
 }
 
+func getScraper(tweetUserName string, tweetPassword string, tweetCodeFor2fa string) *twitterscraper.Scraper {
+	cookiePath := fmt.Sprintf(".cookies%clogin_cookie_%s_%s", os.PathSeparator, tweetUserName, tweetCodeFor2fa)
+
+	scraper := twitterscraper.New()
+
+	fr, err := os.Open(cookiePath)
+	if err != nil {
+		fmt.Println("tweetUserName:", tweetUserName)
+		fmt.Println("tweetPassword:", tweetPassword)
+		fmt.Println("tweetCodeFor2fa:", tweetCodeFor2fa)
+		err := scraper.Login(tweetUserName, tweetPassword, tweetCodeFor2fa)
+		if err != nil {
+			log.Fatal("Twitter login failed:", err.Error())
+		}
+
+		cookies := scraper.GetCookies()
+		content, _ := json.Marshal(cookies)
+		fw, _ := os.Create(cookiePath)
+		fw.Write(content)
+
+		return scraper
+	}
+
+	var cookies []*http.Cookie
+	json.NewDecoder(fr).Decode(&cookies)
+	scraper.SetCookies(cookies)
+
+	if !scraper.IsLoggedIn() {
+		err := scraper.Login(tweetUserName, tweetPassword, tweetCodeFor2fa)
+		if err != nil {
+			log.Fatal("Twitter login failed:", err.Error())
+		}
+
+		cookies := scraper.GetCookies()
+		content, _ := json.Marshal(cookies)
+		fw, _ := os.Create(cookiePath)
+		fw.Write(content)
+
+		return scraper
+	}
+
+	return scraper
+}
+
 func tickerScan() {
-	tweetQuery := os.Getenv("TWEET_QUERY")
-	if tweetQuery == "" {
-		fmt.Println("Miss env: [tweetQuery]")
+	tweetUserName := os.Getenv("TWEET_USER_NAME")
+	if tweetUserName == "" {
+		fmt.Println("Miss env: [TWEET_USER_NAME]")
 		return
 	}
+	tweetPassword := os.Getenv("TWEET_PASSWORD")
+	if tweetPassword == "" {
+		fmt.Println("Miss env: [TWEET_PASSWORD]")
+		return
+	}
+	tweetCodeFor2fa := os.Getenv("TWEET_CODE_FOR_2FA")
+	if tweetCodeFor2fa == "" {
+		fmt.Println("Miss env: [TWEET_CODE_FOR_2FA]")
+		return
+	}
+	tweetQuery := os.Getenv("TWEET_QUERY")
+	if tweetQuery == "" {
+		fmt.Println("Miss env: [TWEET_QUERY]")
+		return
+	}
+
+	scraper := getScraper(tweetUserName, tweetPassword, tweetCodeFor2fa)
+	scraper.SetSearchMode(twitterscraper.SearchLatest)
 
 	var scanTotal int64
 	for {
 		log.Println("scanTotal:", scanTotal)
 
-		scan(tweetQuery)
-		time.Sleep(time.Second * 5)
+		scan(scraper, tweetQuery)
+		time.Sleep(time.Second * 10)
 
 		scanTotal += 1
 	}
